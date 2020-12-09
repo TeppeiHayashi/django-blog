8c59e486c92e0a157c1ea158c35e80258c8611f3
github Teppei Hayashi <th6221@icloud.com>
2020-12-09 16:07:05 +0900

    Middlewareにwhitenoiseを追加

--- project03/settings/production.py
+++ project03/settings/production.py
@@ -28,3 +28,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
+
+import django_heroku
+django_heroku.settings(locals())
