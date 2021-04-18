f61ca573c96499c70962a8c7220c1a310387ba0b
Teppei Hayashi <th6221@icloud.com>
2021-04-18 18:13:21 +0900

    projectディレクトリの名前を変更

--- config/settings/base.py
+++ config/settings/base.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-ROOT_URLCONF = 'project03.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'project03.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
